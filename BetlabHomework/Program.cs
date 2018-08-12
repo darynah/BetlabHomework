ec96cb303952d2e8b491f49d92d8b8e58d3d7229
Darina Gorobets <darina.gorobets@gmail.com>
2018-08-12 14:25:09 +0300

    commit

---
+++ BetlabHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BetlabHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Readlines("playlist.csv");
+            Console.ReadLine();
+
+        }
+
+        private static void Readlines(string filename)
+        {
+           var input = int.Parse(Console.ReadLine());
+           List<string> list = File.ReadAllLines("playlist.csv").ToList();
+           Console.Write(list[input]);
+           
+           
+        }
+    }
+}
---
+++ ConditionalStatementsСycles/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConditionalStatementsСycles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "mghhhopheylalaley";
+            char[] array = input.ToCharArray();
+            var dictionary = new Dictionary<char, int>();
+            foreach (var ch in array)
+            {
+                if (!dictionary.ContainsKey(ch))
+                    dictionary[ch] = 1;
+                else
+                {
+                    dictionary[ch] = dictionary[ch] + 1;
+                }
+            }
+            char x = 'o';
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value == 1)
+                {
+                    x = pair.Key;
+                    int indexofchar = input.IndexOf(x);
+                    Console.WriteLine(indexofchar + " " + x);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+
+        /*static void Main(string[] args)
+        {
+            string input = "mghhhopheylalaley";
+
+            var singleChar = input.GroupBy(z => z).First(y => y.Count() == 1).Key;
+            Console.WriteLine(input.IndexOf(singleChar));
+            Console.ReadLine();
+        }*/
+
+    }
+}
