3af574383735498d2b316f2c2c43b07442b1a408
Darina Gorobets <darina.gorobets@gmail.com>
2018-09-25 22:48:33 +0300

    add TryParse

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,31 +6,45 @@ namespace Calculator
     {
         static double number1;
         static double number2;
-        static string operation;
+        static double operation;
         static void Main(string[] args)
         {
             Console.Write("Введите число 1: ");
-            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number1 = ReadFromKey(Console.ReadLine());
             Console.Write("Введите число 2: ");
-            double number2 = Convert.ToDouble(Console.ReadLine());
+            double number2 = ReadFromKey(Console.ReadLine());
             Console.Write("Введите операцию: ");
             string operation = Console.ReadLine();
-            Print(SuperCalc( number1,  number2,  operation));
+            Print(SuperCalc(number1, number2, operation));
             Console.ReadLine();
         }
+        public static double ReadFromKey(string input)
+        {
+            double number;
+            while (!Double.TryParse(input, out number))
+            {
+                if (!Double.TryParse(input, out number))
+                    Console.WriteLine("Incorrect number!");
+                break;
+            }
+            return number;
+        }
+
         public static double SuperCalc(double number1, double number2, string operation)
         {
             double result = 0;
+            var flag = 0;
             if (operation == "+")
                 result = number1 + number2;
             else if (operation == "-")
                 result = number1 - number2;
-            else Console.WriteLine("Вы ввели неправильную операцию");
+            else
+                Console.WriteLine("Вы ввели неправильную операцию");
             return result;
         }
         public static void Print(double result)
         {
-            Console.WriteLine("Pезультат: {0}", result);
+           Console.WriteLine("Pезультат: {0}", result);
         }
     }
 }
